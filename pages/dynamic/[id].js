514ff4872b1dfa989bed0f4f99219b08e80024fa
CodeAgony <yaroslav.bkh@gmail.com>
2020-09-13 18:58:55 +0300

    Use dynamic id passed to getInitialProps to grab data. Awesome

--- pages/dynamic/[id].js
+++ pages/dynamic/[id].js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { useRouter } from "next/router"
 
-export const DynamicPage = () => {
+export const DynamicPage = ({login}) => {
 const router = useRouter();
 console.log("DynamicPage -> router", router)
 
   return (
     <div>
-      dynamic page
+      Contibutor - {login}
     </div>
   )
 }
+
+DynamicPage.getInitialProps = async (ctx) => {
+  console.log("Test.getInitialProps -> ctx", ctx)
+  const { query } = ctx;
+  console.log("DynamicPage.getInitialProps -> query", query)
+  debugger
+    const res = await fetch(`https://api.github.com/repos/CodeAgony/${query.id}/contributors`)
+    const json = await res.json()
+    console.log("DynamicPage.getInitialProps -> json", json)
+
+    return { login: json[0].login }
+  }
 
 export default DynamicPage
